462afd8a19eed026a8ded24fff0521447fb5276d
AlhHabibah <86188075+AlhHabibah@users.noreply.github.com>
2021-08-14 08:27:20 +0300

    Update auth.py

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 import os
 
-AUTH0_DOMAIN = environ.get('AUTH0_DOMAIN')    # AUTH0_DOMAIN='Habibah.auth0.com'
+AUTH0_DOMAIN = environ.get('AUTH0_DOMAIN')    # AUTH0_DOMAIN='fsnd-habibah.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = environ.get('API_AUDIENCE') # API_AUDIENCE= 'Coffee'
+API_AUDIENCE = environ.get('API_AUDIENCE') # API_AUDIENCE= 'Coffee-Shop'
 
 ## AuthError Exception
 '''
