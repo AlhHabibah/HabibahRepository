296ec2c631496762f8088332bb5be845bb166209
AlhHabibah <86188075+AlhHabibah@users.noreply.github.com>
2021-08-14 08:26:13 +0300

    Update environment.ts

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsnd-habibah.us', // the auth0 domain prefix
+    audience: 'Coffee-Shop', // the audience set for the auth0 app
+    clientId: 'TyW3HeImMOfVn54oolbBbpzEfkH4iNjK', // the client id generated for the auth0 app
+    callbackURL: 'https://localhost:4200/login-results', // the base url of the running ionic application. 
   }
 };
